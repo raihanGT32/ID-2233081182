8419a518d3e8a413534c3fbab3099498f82cb6a4
Raihan Jamil <148842041+raihanGT32@users.noreply.github.com>
2023-11-06 19:58:11 +0600

    Add files via upload

---
+++ array multiFunc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+int multiplyArray(int arr[], int size) {
+    int result = 1;
+
+    for (int i = 0; i < size; i++) {
+        result =result* arr[i];
+    }
+
+    return result;
+}
+
+int main() {
+    int size;
+
+    printf("Enter the size of Array: ");
+    scanf("%d", &size);
+
+
+
+    int array[size];
+
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    int result = multiplyArray(array, size);
+
+    printf("Result of multiplying array is: %d\n", result);
+
+    return 0;
+}
